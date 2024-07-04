6e967d525e7875785f84408596dc85b5cde47f5c
Lee Jinsol <n.solee9@gmail.com>
2024-07-04 13:51:39 +0900

    refactor: create DTOs

--- src/main/java/com/helloworld/quickstart/controller/QuickController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -2,12 +2,17 @@ package com.helloworld.quickstart.controller;
 
 import com.helloworld.quickstart.dto.ItemDto;
 import com.helloworld.quickstart.dto.ResponseDto;
+import com.helloworld.quickstart.service.QuickService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 public class QuickController {
+
+    @Autowired
+    private QuickService quickService;
     @GetMapping("/dummy")
     public String dummy() {
         log.info("dummy");
@@ -37,8 +42,15 @@ public class QuickController {
     @PostMapping("/item")
     public ResponseDto registerItem(@RequestBody ItemDto item) {
         log.info("item: {}", item);
+
+        boolean b = quickService.registerItem(item);
+        if(b == true) {
+            ResponseDto responseDto = new ResponseDto();
+            responseDto.setMessage("ok");
+            return responseDto;
+        }
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setMessage("ok");
+        responseDto.setMessage("fail");
         return responseDto;
     }
 }
