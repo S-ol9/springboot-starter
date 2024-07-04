d4c4bb2b02c26b75b706dfecb9f730132a5ffcae
Lee Jinsol <n.solee9@gmail.com>
2024-07-04 12:37:46 +0900

    feat: add GetMapping for member, company in controller

--- src/main/java/com/helloworld/quickstart/controller/QuickController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -1,17 +1,38 @@
 package com.helloworld.quickstart.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class QuickController {
     @GetMapping("/dummy")
     public String dummy() {
+        log.info("dummy");
         return "dummy";
     }
 
     @GetMapping("/dummy2")
     public String dummy2() {
+        log.info("dummy");
         return "dummy2";
     }
+
+    @GetMapping("/member")
+    public String getMember(@RequestParam("empNo") String empNo
+            , @RequestParam("year") int year) {
+        log.info("empNo: {}", empNo);
+        log.info("yearL {}", year);
+        return "ok";
+    }
+
+    @GetMapping("company/{id}")
+    public String getCompany(@PathVariable("id") String id) {
+        log.info("id: {} ", id);
+        return "ok";
+    }
+
 }
